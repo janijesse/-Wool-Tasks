89db362e1b8f7ef9867cf90b61c27adfc271a0a1
janijesse <jani@Mac-mini-de-Jani.local>
2024-09-03 14:55:56 +0200

    basic tasks manager

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return <footer className="text-center py-6 font-medium">Wool-Tasks</footer>;
+};
+
+export default Footer;
---
+++ src/components/TasksSection/TaskItem/InfosTask.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Task } from "../../../interfaces";
+import { ReactComponent as Calendar } from "../../../assets/date.svg";
+import useDate from "../../hooks/useDate";
+
+const InfosTask: React.FC<{ task: Task; isListInView1: boolean }> = ({
+  task,
+  isListInView1,
+}) => {
+  const dateFormated = useDate(task.date);
+
+  return (
+    <div className={`flex flex-col flex-1 ${isListInView1 ? "mr-6" : ""}`}>
+      <div
+        className={`flex items-center justify-between ${
+          isListInView1 ? "mb-1" : "mb-2"
+        }`}
+      >
+        <span className="block font-medium dark:text-slate-200">
+          {task.title}
+        </span>
+      </div>
+      <p
+        title={task.description}
+        className={`description mb-2 text-slate-500 dark:text-slate-500 ${
+          isListInView1 ? "line-clamp-2 sm:line-clamp-1" : "line-clamp-3"
+        }`}
+      >
+        {task.description}
+      </p>
+    
+    </div>
+  );
+};
+
+export default InfosTask;
